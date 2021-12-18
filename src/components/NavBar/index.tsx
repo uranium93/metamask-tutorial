2d8fdb1f43ed55b04b2c446210fe9f1b222c46df
uranium93 <19hanafi93@gmail.com>
2021-12-18 12:32:13 +0100

    chore: add comments

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -28,9 +28,11 @@ const NavBar: React.FC<unknown> = () => {
   };
 
   const metamaskStatus = async (provider: ethers.providers.Web3Provider) => {
+    // Try to get list if accounts from metamask, return empty [] if not connected
     const accounts = await provider.listAccounts();
     if (accounts.length) {
       setWeb3Status(Web3Status.CONNECTED);
+      // First account in the array {index 0} is always the selected account in metamask
       const address = accounts[0];
       setAccountAddress(`${address.slice(0, 6)}...${address.slice(-4)}`);
       return;
@@ -39,6 +41,34 @@ const NavBar: React.FC<unknown> = () => {
     setAccountAddress(`Connect`);
   };
 
+  useEffect(() => {
+    // Metamask inject "ethereum" object window for each page you visit, If installed.
+    const { ethereum } = window;
+    // If ethereum is undefined, means metamask is not installed.
+    if (!ethereum) {
+      setWeb3Status(Web3Status.NOT_FOUND);
+      return;
+    }
+    /**
+     * @summary Request metamask to ask user to switch network.
+     * @see https://docs.metamask.io/guide/ethereum-provider.html#chain-ids
+     */
+    window.ethereum
+      .request({
+        method: `wallet_switchEthereumChain`,
+        params: [{ chainId: isProductionEnv() ? `0x1` : `0x4` }],
+      })
+      .catch(console.error);
+
+    // Check if metamask is connected to update UI status, if connected display wallet address
+    metamaskStatus(new ethers.providers.Web3Provider(ethereum));
+  }, []);
+
+  /**
+   * @summary Account events
+   * @event accountChanged User changed account
+   * @event chainChanged User changed chain Exp: ETH mainnet => Fantom mainnet
+   */
   useEffect(() => {
     window.ethereum?.on(`accountsChanged`, (accounts) => {
       if (accounts.length) {
@@ -53,25 +83,6 @@ const NavBar: React.FC<unknown> = () => {
     window.ethereum?.on(`chainChanged`, () => {
       window.location.reload();
     });
-  }, []);
-
-  useEffect(() => {
-    const { ethereum } = window;
-    if (!ethereum) {
-      setWeb3Status(Web3Status.NOT_FOUND);
-      return;
-    }
-    /**
-     * @see https://docs.metamask.io/guide/ethereum-provider.html#chain-ids
-     */
-    window.ethereum
-      .request({
-        method: `wallet_switchEthereumChain`,
-        params: [{ chainId: isProductionEnv() ? `0x1` : `0x4` }],
-      })
-      .catch(console.error);
-
-    metamaskStatus(new ethers.providers.Web3Provider(ethereum));
   }, []);
 
   const connectHandler = async (
@@ -91,7 +102,16 @@ const NavBar: React.FC<unknown> = () => {
       });
       return;
     }
+    /**
+     * @description if metamask is already connected return (break code)
+     * the way we know if metamask is connected is by checking if there's an account
+     * is by requesting the list of accounts connected with our app {listAccounts}
+     */
     if (web3Status === Web3Status.CONNECTED) return;
+    /**
+     * @description call {eth_requestAccounts} rpc metamask api which tell metamask to
+     * request connecting with our application (Authorization) [User have to approve by clicking confirm]
+     */
     try {
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       await provider.send(`eth_requestAccounts`, []);
